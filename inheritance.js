ec76685964426d8a57ffbb9c8b7c5730b1e5338d
SJKed <simonjurstedtskolan@gmail.com>
2022-04-20 06:43:50 +0200

    added comments to inheritance.js for better clarity

--- inheritance.js
+++ inheritance.js
@@ -15,10 +15,12 @@ class Professor extends Person {
         this.teaches = teaches;
     }
 
+    //Overwrites the introduceSelf method from the Person class
     introduceSelf() {
         console.log(`Hi, I'm ${this.firstName} ${this.lastName} and I teach ${this.teaches}`);
     }
 
+    //Function specific to the Professor class
     grade(paper) {
         const grade = Math.random() * 5;
         console.log(grade);
